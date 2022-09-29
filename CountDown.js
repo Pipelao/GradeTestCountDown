dd39857aad196be6428e6cbf0ac060f0f9db436c
Pipelao <fcjimenez@uc.cl>
2022-09-28 23:50:29 -0300

    Riveros

---
+++ CountDown.js
@@ -0,0 +1,36 @@
+const FinalTime = Date.UTC(2022, 8, 29, 16, 0, 0)/1000
+
+const Now = Math.trunc(Date.now()/1000)
+
+let TimeLeft = FinalTime - Now
+
+const h1 = document.getElementById("CountDown")
+
+const button = document.querySelector("button")
+
+const audio = document.querySelector("audio")
+
+const FormatTimeRemaining = (seconds) => {
+
+    const Hours = Math.trunc(seconds/3600)
+    const Minutes = Math.trunc((seconds%3600)/60)
+    const Seconds = (seconds%3600)%60
+
+    TimeLeft--
+
+    return [Hours, Minutes, Seconds]
+}
+
+const RenderTime = () => {
+    const time = FormatTimeRemaining(TimeLeft)
+
+    h1.innerHTML = `${time[0].toString().padStart(2,"0")}:${time[1].toString().padStart(2,"0")}:${time[2].toString().padStart(2,"0")}`
+}
+
+const Interval = setInterval(RenderTime, 1000)
+
+button.addEventListener('click', () => {
+    audio.play()
+    button.remove()
+    h1.classList = []
+})
