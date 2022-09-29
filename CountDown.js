61a5c3e2c3f1f23499d23689fb67636c115a4860
Pipelao <fcjimenez@uc.cl>
2022-09-29 00:22:26 -0300

    el 0 0

--- CountDown.js
+++ CountDown.js
@@ -12,6 +12,10 @@ const audio = document.querySelector("audio")
 
 const FormatTimeRemaining = (seconds) => {
 
+    if (seconds <= 0) {
+        return [0, 0, 0]
+    }
+
     const Hours = Math.trunc(seconds/3600)
     const Minutes = Math.trunc((seconds%3600)/60)
     const Seconds = (seconds%3600)%60
